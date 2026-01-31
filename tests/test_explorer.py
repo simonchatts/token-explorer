3f994d9a7f9f5da9be1b2ff65370be5352549576
Simon Chatterjee <code@chatts.net>
2026-01-31 14:10:23 +0000

    Fix tests after simplification

--- tests/test_explorer.py
+++ tests/test_explorer.py
@@ -12,23 +12,8 @@ def test_get_top_n_tokens():
     explorer.set_prompt("Hello, world")
     tokens = explorer.get_top_n_tokens(n=5)
     assert len(tokens) == 5
-    assert tokens[0]["token"] == "!"
-
-def test_guide():
-    explorer = Explorer()
-    explorer.set_prompt("Hello, world")
-    explorer.set_guide("ba+")
-    tokens = explorer.get_top_n_tokens(n=5)
-    assert len(tokens) == 2 # actually only 2 valid tokens
-    assert tokens[0]["token"][0] == "b"
-
-def test_guide_append_token():
-    explorer = Explorer()
-    tokenizer = explorer.tokenizer
-    explorer.set_prompt("Hello, world")
-    explorer.set_guide("abc")
-    tokens = explorer.get_top_n_tokens(n=5)
-    assert tokens[0]["token"][0] == "a"
-    explorer.append_token(tokenizer.encode("a")[0])
-    tokens = explorer.get_top_n_tokens(n=5)
-    assert tokens[0]["token"][0] == "b"
+    assert all("token" in token for token in tokens)
+    assert all("token_id" in token for token in tokens)
+    assert all("probability" in token for token in tokens)
+    probabilities = [token["probability"] for token in tokens]
+    assert probabilities == sorted(probabilities, reverse=True)
